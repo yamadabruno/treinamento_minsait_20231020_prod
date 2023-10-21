5db1a62b38d64f6c49744f6e9c4ce0161f2f3b32
yamadabruno <bruno_yamada@hotmail.com>
2023-10-21 14:40:10 -0300

    Salvando alteração 3

--- src/main/java/br/com/brunoyamada/AppProdutos/model/Estoque.java
+++ src/main/java/br/com/brunoyamada/AppProdutos/model/Estoque.java
@@ -2,10 +2,30 @@ package br.com.brunoyamada.AppProdutos.model;
 
 import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "estoque")
 public class Estoque {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "produto_id", referencedColumnName = "id")
 	private Produto produto;
+	
+	@Column(nullable = false)
 	private Integer quantidade;
 
 	public Estoque() {
@@ -15,6 +35,14 @@ public class Estoque {
 		this.id = id;
 		this.produto = produto;
 		this.quantidade = quantidade;
+	}
+	
+	public void adicionarEstoque(int quantidade) {
+		this.quantidade += quantidade; 
+	}
+	
+	public void removerEstoque(int quantidade) {
+		this.quantidade -= quantidade; 
 	}
 
 	public Long getId() {
--- src/main/java/br/com/brunoyamada/AppProdutos/model/Produto.java
+++ src/main/java/br/com/brunoyamada/AppProdutos/model/Produto.java
@@ -3,17 +3,36 @@ package br.com.brunoyamada.AppProdutos.model;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
 public class Produto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false, unique = true)
 	private String codigoBarras;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
 	private BigDecimal preco;
 	
 	public Produto() {
 	}
 
-	public Produto(Long id, String codigoBarras, BigDecimal preco) {
+	public Produto(Long id, String codigoBarras, String nome, BigDecimal preco) {
 		this.id = id;
+		this.nome = nome;
 		this.codigoBarras = codigoBarras;
 		this.preco = preco;
 	}
@@ -32,6 +51,14 @@ public class Produto {
 
 	public void setCodigoBarras(String codigoBarras) {
 		this.codigoBarras = codigoBarras;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public BigDecimal getPreco() {
