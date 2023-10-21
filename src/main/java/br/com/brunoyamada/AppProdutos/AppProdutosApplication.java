7cf79226c23334f6658f179852350d384be94a7d
yamadabruno <bruno_yamada@hotmail.com>
2023-10-21 13:57:11 -0300

    first commit

---
+++ src/main/java/br/com/brunoyamada/AppProdutos/AppProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunoyamada.AppProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunoyamada/AppProdutos/AppProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoyamada.AppProdutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
